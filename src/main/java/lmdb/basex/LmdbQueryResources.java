452ac6067e8760dc724297cd2aeddb9e7d54b97d
Mauricio S Castro <mauricioscastro@hotmail.com>
2015-11-17 11:41:27 -0200

    start moving to gradle

--- src/main/java/lmdb/basex/LmdbQueryResources.java
+++ src/main/java/lmdb/basex/LmdbQueryResources.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class LmdbQueryResources extends QueryResources {
 
     ArrayList<Data> data = new ArrayList<Data>();
